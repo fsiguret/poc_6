1a372c1913fa339e0035957f8d3ad1340d8a45a5
fsiguret <fl.si@orange.fr>
2020-06-23 18:56:49 +0200

    add security for upload images and change rename files

--- backend/middleware/multer-configuration.js
+++ backend/middleware/multer-configuration.js
@@ -1,12 +1,27 @@
 const multer = require('multer');
 
+const MIME_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpg',
+    'image/jpg': 'jpg'
+}
+
 const storage = multer.diskStorage({
    destination: (req, file, callback) => {
-       callback(null, 'images');
+       //Verify if the mime type is valid
+       const isValid = MIME_TYPE_MAP[file.mimetype];
+       let error = new Error("The MIME type is invalid, please use .png, .jpg or .jpeg");
+
+       if(isValid) {
+           error = null;
+       }
+       callback(error, 'images');
    },
    filename: (req, file, callback) => {
-        const nameFile = file.originalname.split(' ').join('_');
-        callback(null, Date.now() + nameFile);
+        let nameFile = file.originalname.substring(0,file.originalname.lastIndexOf('.'));
+        const name = nameFile.toLowerCase().split(' ').join('_');
+        const ext = MIME_TYPE_MAP[file.mimetype];
+        callback(null, name + "_" + Date.now() + "." + ext);
    }
 });
 
