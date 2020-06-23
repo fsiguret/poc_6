67ade173a339b227546bc2dec9156d2f0dc6338d
fsiguret <fl.si@orange.fr>
2020-06-23 18:18:04 +0200

    change save name files

--- backend/middleware/multer-configuration.js
+++ backend/middleware/multer-configuration.js
@@ -1,19 +1,12 @@
 const multer = require('multer');
 
-const mimeTypes = {
-    'image/png': 'png',
-    'image/jpg': 'jpg',
-    'image/jpeg': 'jpg'
-};
-
 const storage = multer.diskStorage({
    destination: (req, file, callback) => {
        callback(null, 'images');
    },
    filename: (req, file, callback) => {
         const nameFile = file.originalname.split(' ').join('_');
-        const extension = mimeTypes[file.mimetype];
-        callback(null, nameFile + Date.now() + '.' + extension);
+        callback(null, Date.now() + nameFile);
    }
 });
 
